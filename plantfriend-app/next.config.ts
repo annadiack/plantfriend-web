d2e0714fb9c3c94f781fba349e6caaad0c62de09
annadiack <46572304+annadiack@users.noreply.github.com>
2026-02-17 21:27:32 +0000

    Add turbopack config and remove any webpack config

--- plantfriend-app/next.config.ts
+++ plantfriend-app/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  turbopack: {}, // <- silences the Next 16 turbopack/webpack warning
 };
 
 export default nextConfig;
